49fd8ddcb51be26a8cf1193508c201fb233c3270
NOEL318 <noelrinaya318@gmail.com>
2022-07-20 20:45:55 -0500

    multi-lang fixed, fiu fiu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import "./App.css";
 import "./App.scss"
+import React, { useState } from "react";
 import {Test, Test2} from "./components/Test";
 import {Message} from "./components/Message";
 import {ClickHandler} from "./components/ClickHandler";
 import { useTranslation } from "react-i18next";
 
+
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n} = useTranslation();
+
+  const chnglng = event => {
+  const lang = event.target.value;
+  console.log(lang)
+  i18n.changeLanguage(lang)
+}
 
   return (
     <div className="App">
 
         <p>{t("home:select_theme")}</p>
+        <select onChange={chnglng} value={i18n.language}>
+
+        <option onClick={()=>i18n.changeLanguage('ru')}>ru</option>
+        <option onClick={()=>i18n.changeLanguage('es')}>es</option>
+        <option onClick={()=>i18n.changeLanguage('en')}>en</option>
+        </select>
+
 
 
 
